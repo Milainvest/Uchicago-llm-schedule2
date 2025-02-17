2dfeca00bb9ad2dfd3bd8fbfdbddf83bb5b8fd8f
河野太郎 <kounotarou@Taros-MacBook-Air-13.local>
2025-02-16 19:27:12 -0600

    fixed errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
 import Head from 'next/head';
 import CourseList from '../components/CourseList';
 import InitialCourseSelection from '../components/InitialCourseSelection';
